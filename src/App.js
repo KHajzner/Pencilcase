22c5a0abeec4a1b60b939a7a986ad92007032782
Kalina <k.hajzner1@ncl.ac.uk>
2024-12-19 23:02:03 +0000

    Set up dnd-kit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, {useState} from 'react';
+import {DndContext} from '@dnd-kit/core';
 
-function App() {
-  return (
-    <div>Online Pencilcase
-    </div>
+import {Container} from './Container/Container';
+import {Pan} from './Pan/Pan';
+
+const App = () => {
+  const containers = ['A', 'B', 'C'];
+  const [parent, setParent] = useState(null);
+  const draggableMarkup = (
+    <Pan id="draggable">Drag me</Pan>
   );
-}
 
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {parent === null ? draggableMarkup : null}
+
+      {containers.map((id) => (
+        // We updated the Droppable component so it would accept an `id`
+        // prop and pass it to `useDroppable`
+        <Container key={id} id={id}>
+          {parent === id ? draggableMarkup : 'Drop here'}
+        </Container>
+      ))}
+    </DndContext>
+  );
+
+  function handleDragEnd(event) {
+    const {over} = event;
+
+    // If the item is dropped over a container, set it as the parent
+    // otherwise reset the parent to `null`
+    setParent(over ? over.id : null);
+  }
+};
 export default App;
