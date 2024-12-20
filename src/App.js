86439d889710648704f850c17eaed32b9d85cbea
Kalina <k.hajzner1@ncl.ac.uk>
2024-12-20 20:27:47 +0000

    Basic palette structure

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React, {useState} from 'react';
-import {DndContext} from '@dnd-kit/core';
 
-import Container from './Container/Container';
-import Pan from './Pan/Pan';
-
+import Palette from './Palette/Palette';
+import palettes from  './catalogue/palettes.json'
 const App = () => {
-  const containers = ['A', 'B', 'C'];
-  const [parent, setParent] = useState(null);
-  const draggableMarkup = (
-    <Pan id="draggable">Drag me</Pan>
-  );
 
+  console.log(palettes);
+  
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {parent === null ? draggableMarkup : null}
-
-      {containers.map((id) => (
-        // We updated the Droppable component so it would accept an `id`
-        // prop and pass it to `useDroppable`
-        <Container key={id} id={id}>
-          {parent === id ? draggableMarkup : 'Drop here'}
-        </Container>
+    <>
+      {palettes.map((palette) => (
+        <Palette key={palette.name} paletteInstance={palette}/>
       ))}
-    </DndContext>
+    </>
+
   );
 
-  function handleDragEnd(event) {
-    const {over} = event;
-
-    // If the item is dropped over a container, set it as the parent
-    // otherwise reset the parent to `null`
-    setParent(over ? over.id : null);
-  }
 };
 export default App;
